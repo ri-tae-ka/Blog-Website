7b2d7a0c8276104401d1f98363e79001d90e1831
ri-tae-ka <ritzthareja@gmail.com>
2022-02-13 11:13:06 +0530

    added final files

--- app.js
+++ app.js
@@ -111,8 +111,11 @@ app.get("/posts/:postId", function(req, res) {
 
 
 
-const PORT = 3000;
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(PORT, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started on port " + port);
 });
